e341873aac75b05d9279e4bcc1b12936940fb8c3
Dipesh Yadav <73176903+techboydk@users.noreply.github.com>
2022-11-26 21:53:56 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,4 @@
-addEventListener("click", function() {
-    var
-          el = document.documentElement
-        , rfs =
-               el.requestFullScreen
-            || el.webkitRequestFullScreen
-            || el.mozRequestFullScreen
-    ;
-    rfs.call(el);
-});
+
 document.addEventListener('scroll', ()=>{
   if (window.scrollY>200) {
     document.querySelector('.search-bar').classList.add('sticky');
